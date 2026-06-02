d9a6f545f57af5d4c3ad5d29364f64125159d4ac
5eqn <subcat2077@gmail.com>
2026-06-02 19:57:14 +0800

    Fix module import

--- globe_walking/go1_gym/rewards/eureka_reward.py
+++ globe_walking/go1_gym/rewards/eureka_reward.py
@@ -19,13 +19,11 @@ class EurekaReward():
                              torch.exp(-10.0 * torch.square(diff)))
         return 1.0 * reward
     
-    
     def _reward_orientation(self):
         env = self.env
         horizontal_sq = torch.sum(torch.square(env.projected_gravity[:, :2]), dim=1)
         reward = torch.exp(-horizontal_sq / 0.05)
         return 1.0 * reward
-    
     
     def _reward_contact(self):
         env = self.env
@@ -34,44 +32,42 @@ class EurekaReward():
         contact_fraction = torch.mean(in_contact, dim=1)
         return 1.0 * contact_fraction
     
+    def _reward_centering(self):
+        env = self.env
+        # Horizontal distance between robot base and ball center
+        base_xy = env.base_pos[:, :2]
+        ball_xy = env.object_pos_world_frame[:, :2]
+        dist_sq = torch.sum(torch.square(base_xy - ball_xy), dim=1)
+        reward = torch.exp(-dist_sq / 0.15)
+        return 0.5 * reward
     
     def _reward_action_rate(self):
         env = self.env
         diff = env.actions - env.last_actions
-        return -0.2 * torch.sum(torch.square(diff), dim=1)
-    
-    
-    def _reward_action_jerk(self):
-        env = self.env
-        jerk = env.actions - 2.0 * env.last_actions + env.last_last_actions
-        return -0.1 * torch.sum(torch.square(jerk), dim=1)
-    
+        rate = torch.sum(torch.square(diff), dim=1)
+        return -0.005 * rate
     
     def _reward_joint_velocity(self):
         env = self.env
-        return -0.0005 * torch.sum(torch.square(env.dof_vel), dim=1)
-    
+        vel_sq = torch.sum(torch.square(env.dof_vel), dim=1)
+        return -0.0005 * vel_sq
     
     def _reward_torque(self):
         env = self.env
-        return -0.0001 * torch.sum(torch.square(env.torques), dim=1)
-    
+        torque_sq = torch.sum(torch.square(env.torques), dim=1)
+        return -0.0001 * torque_sq
     
     def _reward_base_velocity(self):
         env = self.env
-        return -0.01 * torch.sum(torch.square(env.base_lin_vel), dim=1)
-    
+        lin_vel_sq = torch.sum(torch.square(env.base_lin_vel), dim=1)
+        return -0.01 * lin_vel_sq
     
     def _reward_base_angular_velocity(self):
         env = self.env
-        return -0.02 * torch.sum(torch.square(env.base_ang_vel), dim=1)
-    
-    
-    def _reward_ball_stability(self):
-        env = self.env
-        ball_vel_sq = torch.sum(torch.square(env.object_lin_vel), dim=1)
-        return -0.1 * ball_vel_sq
+        ang_vel_sq = torch.sum(torch.square(env.base_ang_vel), dim=1)
+        return -0.005 * ang_vel_sq
 
     # Success criteria as episode length
     def compute_success(self):
         return torch.ones_like(self.env.base_pos[:, 2])
+
